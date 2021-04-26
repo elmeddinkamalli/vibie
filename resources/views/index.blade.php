0d823b59d154e60570f8008070079f80b56b56dc
Elmeddin Kamalli <elmeddinkamalli@gmail.com>
2021-04-26 16:36:04 +0400

    msg

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="https://vibie.herokuapp.com/css/app.css">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
         <meta id="csrf_token" name="csrf-token" content="{{ csrf_token() }}" />
     </head>
     <body>
         <div id="app"></div>
         <script>var wavesurfer = window.WaveSurfer;</script>
-        <script src="{{asset('js/app.js')}}"></script>
+        <script src="https://vibie.herokuapp.com/js/app.js"></script>
     </body>
 </html>
