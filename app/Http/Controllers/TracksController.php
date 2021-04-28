27d41ee0471757422a106564b57b6b2993cbe63a
Elmeddin Kamalli <elmeddinkamalli@gmail.com>
2021-04-28 11:25:41 +0400

    mofifications

--- app/Http/Controllers/TracksController.php
+++ app/Http/Controllers/TracksController.php
@@ -118,7 +118,7 @@ class TracksController extends Controller
         if($track){
             $related_tracks = Tracks::where(function ($q) use ($track) {
                 $q->where('user_id', $track['user_id'])->orWhere('genre_id', $track['genre_id']);
-            })->get()->take(20);
+            })->orderByRaw("FIELD(id, ".$track->id.")". " DESC")->get()->take(20);
         }
 
         if(auth()->user()){
